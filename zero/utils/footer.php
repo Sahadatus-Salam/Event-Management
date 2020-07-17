a0ee732589411bd380b3fd08423706522713a51f
Sahadatus-Salam <61723279+Sahadatus-Salam@users.noreply.github.com>
2020-07-17 23:35:22 +0600

    Add files via upload

---
+++ zero/utils/footer.php
@@ -0,0 +1,31 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>Z</strong><span class = "small footerSubtext">ero</span>
+                        <strong>G</strong><span class = "small footerSubtext">ravity</span>
+            
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Sub Area, Andarkilla, Chittagong.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    This is a combined task<br>
+                    Made by :<br>
+			<strong>S</strong><span class = "small footerSubtext">amin</span><br>
+			<strong>J</strong><span class = "small footerSubtext">annath</span><br>
+			<strong>P</strong><span class = "small footerSubtext">unam</span><br>
+			<strong>S</strong><span class = "small footerSubtext">adia</span><br>
+                </div>
+            </section>
+            
+        </div>
+    </div>
+</footer>
